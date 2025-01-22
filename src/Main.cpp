e0f44e34755dbfbf567f4c85a78035443ea9c0a7
Leonardo <leozetti02@gmail.com>
2025-01-22 11:23:02 +0100

    Multi-pass rendering demo

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,7 @@
 #include <GL/glew.h>
-#include<GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 
 #include <iostream>
 #include <vector>
@@ -16,8 +18,6 @@ namespace config
     const int windowHeight = 600;
     const std::string windowTitle = "BoxOfSunlight";
     const std::string shadersDir = "..\\..\\..\\src\\shaders\\";
-    const std::string vertexShaderPath = shadersDir + "VertexShader.vert";
-    const std::string fragmentShaderPath = shadersDir + "FragmentShader.frag";
 }
 
 // Callback function that receives debugging messages from OpenGL
@@ -50,21 +50,14 @@ static void initGL()
     glDebugMessageCallback(debugMessageCallback, 0);
     // Set Viewport
     glViewport(0, 0, config::windowWidth, config::windowHeight);
-
-    std::vector<BOSL::Shader> shaders;
-    shaders.push_back(BOSL::Shader(config::vertexShaderPath, GL_VERTEX_SHADER));
-    shaders.push_back(BOSL::Shader(config::fragmentShaderPath, GL_FRAGMENT_SHADER));
-    BOSL::Program shaderProgram(shaders);
-
-    shaderProgram.use();
 }
 
-static void initGeometry()
+static GLuint initGeometry()
 {
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.0f,  0.5f, 0.0f,
     };
 
     unsigned int VAO;
@@ -73,13 +66,47 @@ static void initGeometry()
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-    
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    return VAO;
+}
+
+unsigned int loadCubemap(std::vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            throw BoxOfSunlightError("Cubemap tex failed to load at path: " + faces[i]);
+            //stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
 
 int main()
@@ -87,13 +114,93 @@ int main()
     try {    
         Window window = Window(config::windowWidth, config::windowHeight, config::windowTitle);
         initGL();
-        initGeometry();
+        GLuint triangleVAO = initGeometry();
+        
+        /*
+        std::vector<std::string> faces =
+        {
+            "right.jpg",
+                "left.jpg",
+                "top.jpg",
+                "bottom.jpg",
+                "front.jpg",
+                "back.jpg"
+        };
+        unsigned int cubemapTexture = loadCubemap(faces);
+        */
+
+        std::vector<BOSL::Shader> baseShaders;
+        baseShaders.push_back(BOSL::Shader(config::shadersDir + "base.vert", GL_VERTEX_SHADER));
+        baseShaders.push_back(BOSL::Shader(config::shadersDir + "base.frag", GL_FRAGMENT_SHADER));
+        BOSL::Program baseShaderProgram(baseShaders);
+
+        std::vector<BOSL::Shader> screenShaders;
+        screenShaders.push_back(BOSL::Shader(config::shadersDir + "screen.vert", GL_VERTEX_SHADER));
+        screenShaders.push_back(BOSL::Shader(config::shadersDir + "screen.frag", GL_FRAGMENT_SHADER));
+        BOSL::Program screenShaderProgram(screenShaders);
+
+        // quad that fills the entire screen in Normalized Device Coordinates
+        float quadVertices[] = { 
+            // positions   // texCoords
+            -1.0f,  1.0f,  0.0f, 1.0f,
+            -1.0f, -1.0f,  0.0f, 0.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+
+            -1.0f,  1.0f,  0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 0.0f,
+             1.0f,  1.0f,  1.0f, 1.0f
+        };
+
+        unsigned int quadVAO, quadVBO;
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+        glBindVertexArray(0);
+
+        GLuint framebuffer;
+        glGenFramebuffers(1, &framebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        
+        GLuint textureColorbuffer;
+        glGenTextures(1, &textureColorbuffer);
+        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, config::windowWidth, config::windowHeight,
+                        0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
 
         while (!window.shouldClose())
         {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            // first pass
+            glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+            glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            baseShaderProgram.use();
+            glBindVertexArray(triangleVAO);
             glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            // second pass
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            screenShaderProgram.use();
+            glBindVertexArray(quadVAO);
+            glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+            
             window.update();
         }
         return EXIT_SUCCESS;
---
+++ src/shaders/screen.frag
@@ -0,0 +1,12 @@
+#version 430 core
+
+in vec2 texCoords;
+
+out vec4 fragColor;
+
+uniform sampler2D screenTexture;
+
+void main()
+{
+	fragColor = texture(screenTexture, texCoords);
+}
